da3635319cd247c639de1d4d309ff0490dd9403c
seong <seong@APRILAPTOP>
2022-06-11 16:03:32 +0900

    Add project files.

---
+++ msdoc img extractor/msdoc img extractor.h
@@ -0,0 +1,32 @@
+
+// msdoc img extractor.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CmsdocimgextractorApp:
+// See msdoc img extractor.cpp for the implementation of this class
+//
+
+class CmsdocimgextractorApp : public CWinApp
+{
+public:
+	CmsdocimgextractorApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CmsdocimgextractorApp theApp;
---
+++ msdoc img extractor/msdoc img extractorDlg.h
@@ -0,0 +1,34 @@
+
+// msdoc img extractorDlg.h : header file
+//
+
+#pragma once
+
+
+// CmsdocimgextractorDlg dialog
+class CmsdocimgextractorDlg : public CDialogEx
+{
+// Construction
+public:
+	CmsdocimgextractorDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MSDOC_IMG_EXTRACTOR_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
